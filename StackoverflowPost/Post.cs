341b708513919d0c95478e7382c2ca6494ac8987
Pedro Lazari <67986647+PedroLazari233@users.noreply.github.com>
2023-05-26 20:15:01 -0300

    initial setup and project

---
+++ StackoverflowPost/Post.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackoverflowPost
+{
+    internal class Post
+    {
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int CurrentVoteValue { get; private set; }
+
+        public void UpVote()
+        {
+            CurrentVoteValue++;
+        }
+
+        public void DownVote()
+        {
+            CurrentVoteValue--;
+        }
+    }
+}
---
+++ StackoverflowPost/Program.cs
@@ -0,0 +1,40 @@
+﻿using StackoverflowPost;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var post = new Post();
+
+        Console.Write("Create a title for a StackOverflow post: ");
+        post.Title = Console.ReadLine();
+
+        Console.Write("Create a description for the same post: ");
+        post.Description = Console.ReadLine();
+
+        while (true)
+        {
+            Console.WriteLine("UpVote by inserting U and DownVote by inserting D: ");
+            var vote = Console.ReadLine();
+
+            switch(vote)
+            {
+                case "U":
+                    post.UpVote(); break;
+                case "D":
+                    post.DownVote(); break;
+                default:
+                    Console.WriteLine("Wrong input!"); break;
+            }
+
+            Console.WriteLine("Do you want to vote again?");
+            var input = Console.ReadLine();
+
+            if (input == "No")
+                break;
+
+        }
+
+        Console.WriteLine("Total votes for the post " + post.Title + " is " + post.CurrentVoteValue);
+    }
+}
